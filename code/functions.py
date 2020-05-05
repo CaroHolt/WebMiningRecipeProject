8ce7ad0d123a5c99a5cd0af6f57249e6f7709366
CaroHolt <57441869+CaroHolt@users.noreply.github.com>
2020-05-05 10:31:48 +0200

    image scrape

--- code/functions.py
+++ code/functions.py
@@ -13,29 +13,30 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Insert list of recipes to recommend
-def get_image_source_url(recipeList):
+def get_image_source_url(recipe_id):
     url = 'https://www.food.com/recipe/'
+    recipe_image_url = ""
+    recipe_id = str(recipe_id)
 
-    for index, row in recipeList.iterrows():
-        recipe_id = str(row['id'])
-        recipe_url = url + recipe_id
+    recipe_url = url + recipe_id
 
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        driver = webdriver.Chrome("C:/Webdrivers/chromedriver",options=options)
-        try:
-            driver.get(recipe_url)
-            images = driver.find_elements_by_tag_name('img')
-            recipe_images = []
-            for image in images:
-                if (image.get_attribute('class') == 'recipe-image__img'):
-                    recipe_images.append(image.get_attribute('src'))
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome("C:/Webdrivers/chromedriver",options=options)
+    try:
+        driver.get(recipe_url)
+        images = driver.find_elements_by_tag_name('img')
+        image_urls = []
 
-            recipeList.at[index, 'ImageURL'] = recipe_images[0]
-        except Exception as err:
-            print("No page found for {} - {}".format(recipe_id, err))
+        for image in images:
+            if (image.get_attribute('class') == 'recipe-image__img'):
+                image_urls.append(image.get_attribute('data-src'))
+        recipe_image_url = image_urls[0]
 
-    return recipeList
+    except Exception as err:
+        print("No page found for {} - {}".format(recipe_id, err))
+
+    return recipe_image_url
 
 #Insert dataframe and name of old and new column as string
 def get_keywords(df, oldColumnName, newColumnName):
