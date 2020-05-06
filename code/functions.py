b4280ff94bcddb6d17007d7545c2c259aeda62db
Yuelingl <39614546+Yuelingl@users.noreply.github.com>
2020-05-06 21:44:48 +0200

    make_all_recommendations()
    + make_all_predictions() & some function comments

--- code/functions.py
+++ code/functions.py
@@ -46,7 +46,7 @@ def get_image_source_url(recipe_id):
 
     return recipe_image_url
 
-#Insert dataframe and name of old and new column as string
+Insert dataframe and name of old and new column as string
 def get_keywords(df, oldColumnName, newColumnName):
     df[newColumnName] = ""
     for index, row in df.iterrows():
@@ -83,52 +83,3 @@ def get_interaction_processed(processed, interactions):
                            .drop(columns=['index'])
   return interactions_processed
 
-# Calculate recommendations from cosine similarity
-def get_recommendation_cos(processed, interactions, recipe_id, user_id, cosine_sim, k):
-    df_sim = pd.DataFrame(cosine_sim, index=processed['recipe_id'], columns=processed['recipe_id'])
-    #adjust to sparse matrix
-    user_rids = interactions[interactions['user_id']==user_id]['recipe_id'].values
-    df_row = df_sim.loc[df_sim.index==recipe_id]
-    df_user = df_row[user_rids]
-    results = list(zip(list(df_user.columns), list(df_user.values[0])))
-    results_ordered = sorted(results, key=lambda x: x[1], reverse=True)
-    results_topk = np.array(results_ordered[1:k+1])
-    return results_topk[:,0]
-
-# Make the prediction of a rating
-def predict_rating(interactions, user_id, recipe_ids):
-    scores = []
-    for rid in recipe_ids:
-        score = interactions.loc[(interactions.user_id==user_id) & (interactions.recipe_id==rid)]['rating']
-        scores.append(score.values[0])
-        return np.mean(scores)
-
-# Retriebe results of cosine similarity
-def get_results_cos(processed, interactions, recipe, recipe_id, user_id, cosine_sim,k):
-    actual = interactions.loc[(interactions.user_id==user_id) & (interactions.recipe_id==recipe_id)]['rating'].values[0]
-    recipe_ids = get_recommendation_cos(processed,interactions,recipe_id,user_id,cosine_sim,k)
-    prediction = predict_rating(interactions, user_id, recipe_ids)
-    return actual, prediction
-
-# Calculate coverage of predicted recipes
-def get_coverage(processed, interactions, recipe, cosine_sim,k):
-    interactions_processed = get_interaction_processed(processed, interactions)
-    uid_sample = interactions_processed['user_id'].values
-    rid_sample = interactions_processed['recipe_id'].values
-
-    all_rids = interactions_processed['recipe_id'].unique()
-    pred_rids = []
-
-    for i in range(len(interactions_processed)):
-        try:
-          recipe_ids = get_recommendation_cos(processed,
-                                                interactions_processed,
-                                                rid_sample[i],
-                                                uid_sample[i],
-                                                cosine_sim,
-                                                k)
-          pred_rids += list(recipe_ids)
-        except:
-          next
-    pred_bids = np.array(list(set(pred_rids)))
-    return len(pred_bids)/len(all_rids)
